18e031b4e8762d45ee8ba534af75dbca4dbe1136
Lena_Bondar <lencha.2005@gmail.com>
2024-08-18 11:34:54 +0300

    update main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,10 @@
+import {creatGalleryCard} from './js/render-functions';
+import {fetchPhotos, loader} from './js/pixabay-api';
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 const formSearchEl = document.querySelector('.js-form-search');
 const galleryList = document.querySelector('.list-gallery');
 
@@ -6,35 +13,41 @@ event.preventDefault();
 
 const inputValue = formSearchEl.elements.user_query.value;
 
-fetch(`https://pixabay.com/api/?key=45478018-6810a6814d3c626f27e622ce2&q=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true`)
-.then(response => {
-    if(!response.ok) {
-        throw new Error(response.status)
-    }
-    // console.dir(response.json());
-    return response.json() ;
-})
+
+fetchPhotos(inputValue)
 .then(data => {
+    galleryList.innerHTML = '';
+        formSearchEl.reset();
+
     if (data.hits.length === 0){
-        alert('Sorry, there are no images matching your search query. Please try again!');
+        iziToast.error({
+            title: 'Error',
+            position: 'topRight',
+            message: 'Sorry, there are no images matching your search query. Please try again!',
+        });
+
         return;
     };
-const creatGalleryCard = imgInfo => {
-        return `
-        <li class="gallery-card">
-        <img class="gallery-img" src="${imgInfo.webformatURL}" alt="${imgInfo.tags}" />
-        </li>`
-    };
-
-    const CalleryList = data.hits.map(img => creatGalleryCard(img)).join('');
-    galleryList.innerHTML = CalleryList;
-
-    console.log(CalleryList);
     
-    console.dir(data);})
-.catch(err => console.log(err));
+    const galleryCardsTemplate = data.hits.map(img => creatGalleryCard(img)).join('');
+    galleryList.innerHTML = galleryCardsTemplate;
+
+    
+    lightbox.refresh();
+
+
+})
+.catch(err => console.log(err))
+.finally(() => {
+    loader.classList.toggle('is-hidden');
+});
 }
 
-
-// console.log('h')
 formSearchEl.addEventListener('submit', onFormSearchSubmit);
+
+const lightbox = new SimpleLightbox('.list-gallery a', { 
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+    overlayOpacity: 0.8
+});
